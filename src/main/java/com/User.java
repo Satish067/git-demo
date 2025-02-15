42811a6b5679571b824f2c3cfa519eaea15bc8a4
Satish067 <sattysatish067@gmail.com>
2025-02-15 16:10:32 +0530

    Adding email to User entity

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -4,6 +4,7 @@ public class User {
     private int userId;
     private String username;
     private String password;
+    private String email;
 
     public int getUserId() {
         return userId;
@@ -27,5 +28,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
